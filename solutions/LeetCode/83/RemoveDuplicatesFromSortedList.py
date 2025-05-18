40a7f40d5139e015edb8e1d6cfedc8670d03aa3c
arataka1313 <arataka1313@gmail.com>
2025-05-18 11:31:40 +0900

    add AtCoder dir

---
+++ solutions/LeetCode/83/RemoveDuplicatesFromSortedList.py
@@ -0,0 +1,17 @@
+from typing import List
+
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+         
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+    
+def main():
+    
+    
+if __name__ == "__main__":
+    main()
